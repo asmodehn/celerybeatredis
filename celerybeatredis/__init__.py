f8b59a13ada9c7ba5b24e34c13999bd76aeed979
alexv <asmodehn@gmail.com>
2016-04-18 21:39:43 +0900

    exposing datetime encoder and decoder. sometimes useful to have available.

--- celerybeatredis/__init__.py
+++ celerybeatredis/__init__.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
 
+from .decoder import DateTimeDecoder, DateTimeEncoder
 from .task import PeriodicTask, Crontab, Interval
 from .schedulers import RedisScheduler, RedisScheduleEntry
 
 __all__ = [
+    'DateTimeDecoder',
+    'DateTimeEncoder',
     'PeriodicTask',
     'Crontab',
     'Interval'
